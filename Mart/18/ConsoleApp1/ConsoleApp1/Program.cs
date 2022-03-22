a821efc15c40752a37e079934361d3fb8de44960
QarayevaVefa <qarayeva.vefa38@mail.ru>
2022-03-22 23:57:13 +0400

    Homework 18 mart

---
+++ Mart/18/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı daxil edin:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı daxil edin:");
+            var nameSurname = Console.ReadLine();
+            Console.WriteLine("Harada yasayırsınız?");
+            var cityName = Console.ReadLine();
+            Console.ReadLine();
+            
+       
+        }
+    }
+}
---
+++ Mart/18/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 sheher adi daxil edin:");
+            var cityName1 = Console.ReadLine();
+            var cityName2 = Console.ReadLine();
+            var cityName3 = Console.ReadLine();
+            var cityName4 = Console.ReadLine();
+            var cityName5 = Console.ReadLine();
+            var cityName6 = Console.ReadLine();
+            var cityName7 = Console.ReadLine();
+            var cityName8 = Console.ReadLine();
+            var cityName9 = Console.ReadLine();
+            var cityName10 = Console.ReadLine();
+            Console.ReadLine();
+          
+            
+
+          
+        }
+    }
+}
