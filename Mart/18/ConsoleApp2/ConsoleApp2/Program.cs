b6f0a79ea642e979c2acb4041038ce52022ea654
QarayevaVefa <qarayeva.vefa38@mail.ru>
2022-03-23 21:08:05 +0400

    add changes

---
+++ Mart/18/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 sheher adi daxil edin:");
+            var nameCity1 = Console.ReadLine();
+            var nameCity2 = Console.ReadLine();
+            var nameCity3 = Console.ReadLine();
+            var nameCity4 = Console.ReadLine();
+            var nameCity5 = Console.ReadLine();
+            var nameCity6 = Console.ReadLine();
+            var nameCity7 = Console.ReadLine();
+            var nameCity8 = Console.ReadLine();
+            var nameCity9 = Console.ReadLine();
+            var nameCity10 = Console.ReadLine();
+            Console.WriteLine(nameCity1 + "," + nameCity2 + "," + nameCity3 + "," + nameCity4 + "," + nameCity5 + "," + nameCity6 + "," + nameCity7 + "," + nameCity8 + "," + nameCity9 + "," + nameCity10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mart/18/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 sheher adi daxil edin:");
+            var nameCity1 = Console.ReadLine();
+            var nameCity2 = Console.ReadLine();
+            var nameCity3 = Console.ReadLine();
+            var nameCity4 = Console.ReadLine();
+            var nameCity5 = Console.ReadLine();
+            var nameCity6 = Console.ReadLine();
+            var nameCity7 = Console.ReadLine();
+            var nameCity8 = Console.ReadLine();
+            var nameCity9 = Console.ReadLine();
+            var nameCity10 = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(nameCity1);
+            Console.WriteLine(nameCity2);
+            Console.WriteLine(nameCity3);
+            Console.WriteLine(nameCity4);
+            Console.WriteLine(nameCity5);
+            Console.WriteLine(nameCity6);
+            Console.WriteLine(nameCity7);
+            Console.WriteLine(nameCity8);
+            Console.WriteLine(nameCity9);
+            Console.WriteLine(nameCity10);
+            Console.ReadLine();
+        }
+    }
+}
