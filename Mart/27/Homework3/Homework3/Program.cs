075a6e01ae2e0b2dcbaaecc88f5cc261ae34fe45
QarayevaVefa <qarayeva.vefa38@mail.ru>
2022-03-29 21:11:29 +0400

    Mart 27 folderi

---
+++ Mart/27/Homework3/Homework3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 2356874;
+            bool booleanVar = true;
+            char charValue = 'B';
+            string stringVar = "Xosh gelmisiniz!";
+            long longValue = 3254687416947551;
+            object objectValue;
+            objectValue  = intValue;
+            objectValue = booleanVar;
+            objectValue = charValue;
+            objectValue = stringVar;
+            objectValue = longValue;
+            
+
+        }
+    }
+}
