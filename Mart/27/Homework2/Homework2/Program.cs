fe3d2978f1dbdf43f9279d23a1a9cc9c25756ae8
QarayevaVefa <qarayeva.vefa38@mail.ru>
2022-03-30 02:04:02 +0400

    to end homeworks

---
+++ Mart/27/Homework2/Homework2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+         C# və ya digər proqramlaşdırma dillərinin dəyişənlərə ad verilərkən bəzi adlandırma qaydaları vardır. Bu qaydalara əməl edilmədiyi təqdirdə proqramımız kompayl olunarkən xəta verəcək. Bu qaydalar aşağıdakılardır:
+         
+            1.Dəyişən adları rəqəmsal ifadələrlə başlaya bilməz.
+            2.Xüsusi xarakter olaraq sadəcə _ istifadə oluna bilər.
+            3.Dəyişən adlarında rəqəmsal xarakterlər istifadə oluna bilər, ancaq ilk hərf rəqəmsal xarakter ola bilməz.
+            4.Proqramlaşdırma dili olaraq ingilis dili nəzərdə tutulduğundan adlandırma zamanı bu dildən istifadə olunması tövsiyə olunur. 
+            5.Dəyişən adları yeganədir.
+            6.Böyük, kiçik hərfə həssasdır.
+            7.Ayrıca dəyişən adları mənalı formada yazılmalıdır. Qısaltma olacaqsa da, məsələn, Adı və Soyadı olaraq fəyişənin adına as yerinə adSoyad yazaraq daha mənalı hala gətirə bilərik.
+            8.Proqramlaşdırma dilində istifadə olunan sözlər dəyişən adı olaraq istifadə olunmur.
+            9.Özel karakterlerden sadece “-” karakteri kullanılabilir.Xüsusi işarələrdən sadəcə "_" (altdan xətt) istifadə oluna bilər.
+ 
+         Və həmçinin kodun daha yaxşı oxunabilir hala gətirilməsi, anlaşılan olması üçün hazırlanmış bəzi standartlar var.
+         Object Name, Namespace name, Class name, Constructor name, Constructor name, Method name, Constants name, Properties name, Delegate name, Enum type name-lərin adlandırılması zamanı PascalCase-dən istifadə olunur.
+         Dəyişən daxilindəki sözlərin baş hərfləri böyük, digər hərfləri isə kiçik olacaq şəkildə yazılır.
+         Method arguments, Local variables, Field name-lərin adlandırılması zamanı isə camelCase-dən istifadə olunur. Dəyişən daxilində ilk hərf kiçik, növbəti sözlərin ilk hərfləri isə böyük olacaq şəkildə yazılır. 
+         HungarionCase quruluş baxımından Pascal kimidi. Sadəcə dəyişən adının qabağında tipini göstərən ön şəkilçi əlavə olunur.
+         (Nümunə; bool dəyişəni üçün => bool bIsActive = true;)
+         UnderscoreCase də Pascal standartına bənzərdi. Fərqi odur ki, sözlərin arasında _ xarakteri qoyulur.  (Nümunə; string Computer_Operator = “Aysel Namazova”;)
+         UpperscoreCase-də bütün hərflər böyük hərflə yazılır və sözlər arasında _ işarəsi istfadə olunur. Ümumiyyətlə sabit olan dəyərlərçün istifadə olunur.
+         (Nümunə; double PI_NUMBER = 3.14;)
+         Bundan əlavə başqa standartlar da mövcuddur. Misal olaraq, SlashCase, SnakeCase, CobraCase, DotCase, CobraCase,
+         DashCase və SeparateCase və s. Yuxarıda adını çəkdiyimiz standart adlandırmalara aid nümunələr yazaq:
+
+            myGreatVariableName-camelCase
+            my/great/variable/name-SlashCase
+            my_great_variable_name-SnakeCase
+            MyGreatVariableName-PascalCase
+            My_Great_Variable_Name-CobraCase
+            my.great.variable.name-DotCase
+            my-great-variable-name-DashCase
+            my great variable name-SeparateCase
+
+
+
+
+           */
+        }
+    }
+}
