599f92c9247a45114d3d6ebccdcef7fdfbbde0e1
QarayevaVefa <qarayeva.vefa38@mail.ru>
2022-03-31 21:06:13 +0400

    Homework's end

--- Mart/27/Homework1,4/Homework1,4/Program.cs
+++ Mart/27/Homework1,4/Homework1,4/Program.cs
@@ -10,7 +10,31 @@ namespace Homework1_4
     {
         static void Main(string[] args)
         {
-            hhjhbvhgvnhgjmb
+            
+            byte minValue = 2;
+            byte maxValue = 203;
+            sbyte minValueNegative = -118;
+            sbyte maxValue1 = 120;
+            short shortValueNegative = -31587;
+            short shortMaxValue = 30564;
+            ushort minaValue1 = 5;
+            ushort maxValue2 = 64327;
+            int intValueNegative = -26344485;
+            int intMaxValue = 2664787946;
+            uint uintMinValue = 12;
+            uint uintMaxValue = 4264448669;
+            long longValueNegative = -154567845632464;
+            long longMaxValue = 15634646789465613446;
+            ulong ulongMinValue = 2413466524;
+            ulong ulongMaxValue = 2364487968489642;
+            float floatValue = 10.5F;
+            double doubleValue = 10.5;
+            decimal decimalValue = 10.5m;
+            char charValue = 'B';
+            char charValue1 = '@';
+            char charValue2 = '2';
+            bool booleanVar = "true";
+            bool booleanVar1 = "false";
 
 
         }
